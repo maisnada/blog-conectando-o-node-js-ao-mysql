d7080dd5b21fdfa5333f286e704f84a8e7d7bd60
Marcelo Palmito <maisnada@gmail.com>
2021-01-10 20:56:20 -0300

    nao refatorado

---
+++ index-sem-refatoracao.js
@@ -0,0 +1,20 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'teste123',
+    database: 'biblioteca'
+});
+
+connection.connect();
+
+connection.query('SELECT * FROM usuario LIMIT 2', function (error, results, fields) {
+
+    if (error) throw error;
+
+    console.log(results);
+
+});
+
+connection.end();
